ca42d6e2f930e4edd5b7e8e3924d8e29c9d1e5c0
VictorFouquet <victor.fouquet@gmail.com>
2023-09-05 13:57:47 +0200

    Add activation functions

---
+++ Activation.cs
@@ -0,0 +1,37 @@
+﻿
+namespace MachineLearning
+{
+    internal class Activation
+    {
+        public static double Relu(double x)
+        {
+            return Math.Max(0, x);
+        }
+
+        public static double ReluDerivative(double x)
+        {
+            return x > 0 ? 1 : 0;
+        }
+
+        public static double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+
+        public static double SigmoidDerivative(double x)
+        {
+            var sigX = Sigmoid(x);
+            return sigX * (1 - sigX);
+        }
+
+        public static double TanH(double x)
+        {
+            return Math.Tanh(x);
+        }
+
+        public static double TanHDerivative(double x)
+        {
+            return 1 - Math.Pow(Math.Tanh(x), 2);
+        }
+    }
+}
