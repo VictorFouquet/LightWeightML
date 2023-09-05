fffacaf24496438b0a77c4d941d40ac240c2b61a
VictorFouquet <victor.fouquet@gmail.com>
2023-09-05 13:58:21 +0200

    Add DataPoint struct

---
+++ DataPoint.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MachineLearning
+{
+    public struct DataPoint
+    {
+        public double[] X;
+        public double[] Y;
+        public DataPoint(double[] X, double[] Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}
