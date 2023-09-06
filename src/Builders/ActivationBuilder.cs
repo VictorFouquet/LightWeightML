c0ab5f5ae1b8a571c94bb9dd549612ab59fd76d3
VictorFouquet <victor.fouquet@gmail.com>
2023-09-06 09:17:59 +0200

    Add builders

---
+++ src/Builders/ActivationBuilder.cs
@@ -0,0 +1,27 @@
+﻿
+using MachineLearning.Enums;
+using MachineLearning.Layers;
+
+namespace MachineLearning.Builders
+{
+    internal static class ActivationBuilder
+    {
+        public static ILayer Get(ActivationEnum layerType)
+        {
+
+            switch (layerType)
+            {
+                case ActivationEnum.Linear:
+                    return new Activation(ActivationFunction.Linear, ActivationFunction.LinearDerivative);
+                case ActivationEnum.TanH:
+                    return new Activation(ActivationFunction.TanH, ActivationFunction.TanHDerivative);
+                case ActivationEnum.Relu:
+                    return new Activation(ActivationFunction.Relu, ActivationFunction.ReluDerivative);
+                case ActivationEnum.Sigmoid:
+                    return new Activation(ActivationFunction.Sigmoid, ActivationFunction.SigmoidDerivative);
+                default:
+                    return new Activation(ActivationFunction.Linear, ActivationFunction.LinearDerivative);
+            }
+        }
+    }
+}
---
+++ src/Builders/LayerBuilder.cs
@@ -0,0 +1,30 @@
+﻿
+using MachineLearning.Enums;
+using MachineLearning.Layers;
+
+namespace MachineLearning.Builders
+{
+    internal static class LayerBuilder
+    {
+        public static ILayer Get(LayerEnum layerType, int numNodesIn = 0, int numNodesOut = 0)
+        {
+            switch (layerType)
+            {
+                case LayerEnum.Dense:
+                    return new Dense(numNodesIn, numNodesOut);
+                case LayerEnum.SoftMax:
+                    return new SoftMax();
+                case LayerEnum.Linear:
+                    return ActivationBuilder.Get(ActivationEnum.Linear);
+                case LayerEnum.Relu:
+                    return ActivationBuilder.Get(ActivationEnum.Relu);
+                case LayerEnum.Sigmoid:
+                    return ActivationBuilder.Get(ActivationEnum.Sigmoid);
+                case LayerEnum.TanH:
+                    return ActivationBuilder.Get(ActivationEnum.TanH);
+                default:
+                    return ActivationBuilder.Get(ActivationEnum.Linear);
+            }
+        }
+    }
+}
